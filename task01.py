acb87b603ef87dfe4f82f8ee4e6e16e56399cbf7
SoujanyaThota27 <soujanyathota1227@gmail.com>
2025-09-01 11:34:10 +0530

    Add files via upload

---
+++ task01.py
@@ -0,0 +1,34 @@
+#find smallest and second smallest number in a list
+'''ip=[10,2,-3,6,-2,3]
+sm=ip[0]
+ssm=ip[0]
+for x in ip:
+    if x<sm:
+        ssm=sm
+        sm=x
+    elif x<ssm and x!=sm:
+        ssm=x
+print("smallest num is",sm)
+print("second smallest number is",ssm)'''
+
+#2.find largest and second largest from given list of numbers.
+'''ip=('sravani','sravan','kumar','kumari','lalitha','lalith','arjun','lakshmi','nandini')
+for i in ip:
+    if i.endswith(("i","a")):
+        print(("female",i))
+    elif i.endswith(("n","h","r")):
+        print(("male",i))'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+# op=[(('sravani','female'),('sravan','male'),('kumar','male'))
