ee8e72c50155e9d26ad2b21045035a35b3dfde7a
SoujanyaThota27 <soujanyathota1227@gmail.com>
2025-09-12 16:58:03 +0530

    Add files via upload

---
+++ task12.py
@@ -0,0 +1,11 @@
+'''n=4
+for i in range(n):
+    for j in range(n):
+        print("*",end="")
+    print()'''
+
+'''n=4
+for i in range(n,-1,-1):
+    for j in range(i,n):
+        print("*",end="")
+    print()'''
