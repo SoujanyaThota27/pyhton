6deb2b34741b9d00e59cef8e35da9805d394af91
SoujanyaThota27 <soujanyathota1227@gmail.com>
2025-09-12 17:58:00 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,18 @@
+#list built-in functions:
+'''1.reverse():
+    The reverse() method in Python is used to reverse the elements of a list in place. 
+    Syntax:
+        l.reverse()'''
+        
+#write internal code for sort(key=len) and clear functions
+'''def sort():
+    l=["hi","srinivas","hello"]
+    l.sort(key=len)
+    print(l)
+sort()   ''' 
+
+'''def clear():
+    a=[6,3,8,1,9,6]
+    a.clear()
+    print(a)
+clear()'''
